bc88d80653479835ba1416122192f5990447e1cd
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-11-15 22:43:54 +0100

    backend initial commit

---
+++ Backend/API/Controllers/LibraryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : Controller
+    {
+        
+    }
+}
